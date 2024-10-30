5694cfe9ec38bc9e75a9de472528ca8e7efa3f84
Arthur Gabriel <arthurgabriel31@hotmail.com>
2024-10-29 22:28:57 -0300

    Adicionado o metodo DELETE

--- src/main/java/br/com/projeto/api/controle/Controle.java
+++ src/main/java/br/com/projeto/api/controle/Controle.java
@@ -2,7 +2,9 @@ package br.com.projeto.api.controle;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,5 +30,10 @@ public class Controle {
     @PutMapping("/")
     public Cliente editar(@RequestBody Cliente c) {
         return acao.save(c);
+    }
+
+    @DeleteMapping("/{codigo}")
+    public void remover(@PathVariable long codigo) {
+        acao.deleteById(codigo);
     }
 }
